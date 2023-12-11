ea1ff84599926cfab98ff8b3371ca9e3f6ba415e
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2023-12-12 00:53:57 +0100

    added domain verifier service interface

---
+++ src/DomainVerifier/Interfaces/IDnsRecordsVerifier.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using DomainVerifier.Settings;
+
+namespace DomainVerifier.Interfaces
+{
+    public interface IDnsRecordsVerifier
+    {
+        /// <summary>
+        /// Verifies that the TXT record is valid.
+        /// </summary>
+        /// <param name="domainName">The domain name to verify the TXT record for.</param>
+        /// <param name="verificationCode">The TXT record to verify.</param>
+        /// <param name="options">Config used to generate the TXT record.</param>
+        /// <returns>True if the TXT record is valid, false otherwise.</returns>
+        Task<bool> IsTxtRecordValidAsync(string domainName, string verificationCode, TxtRecordSettings? options = null);
+
+        /// <summary>
+        /// Verifies that the CNS record is valid.
+        /// </summary>
+        /// <param name="domainName">The domain name to verify the CNS record for.</param>
+        /// <param name="verificationCode">The CNS record to verify.</param>
+        /// <param name="options">Config used to generate the CNS record.</param>
+        /// <returns>True if the CNS record is valid, false otherwise.</returns>
+        Task<bool> IsCnsRecordValidAsync(string domainName, string verificationCode, CnameRecordSettings? options = null);
+    }
+}
