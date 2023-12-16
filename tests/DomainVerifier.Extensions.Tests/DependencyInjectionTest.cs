fff21a03bee874fd469bfd7b5aacd1452f257514
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2023-12-16 13:12:16 +0100

    test(extensions): added DependencyInjection tests

---
+++ tests/DomainVerifier.Extensions.Tests/DependencyInjectionTest.cs
@@ -0,0 +1,63 @@
+using DnsClient;
+using DomainVerifier.Interfaces;
+using DomainVerifier.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DomainVerifier.Extensions.Tests;
+
+public class DependencyInjectionTest
+{
+    private readonly ServiceCollection _services;
+
+    public DependencyInjectionTest()
+    {
+        _services = new ServiceCollection();
+        // load configuration from appsettings.json
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // add IConfiguration to DI container
+        _services.AddSingleton<IConfiguration>(configuration);
+
+        // add DomainVerifier services to DI container
+        _services.AddDomainVerifierServices(configuration);
+    }
+
+
+    [Fact]
+    public void AddDomainVerifierServices_ShouldAddServices()
+    {
+        var serviceProvider = _services.BuildServiceProvider();
+
+        // Assert
+        serviceProvider.GetService<ILookupClient>().Should().NotBeNull();
+        serviceProvider.GetService<IDnsRecordsGenerator>().Should().NotBeNull();
+        serviceProvider.GetService<IDnsRecordsVerifier>().Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddDomainVerifierServices_ShouldInjectDomainVerifierSettings()
+    {
+        var serviceProvider = _services.BuildServiceProvider();
+        var settings = serviceProvider.GetService<DomainVerifierSettings>();
+        var client = serviceProvider.GetService<ILookupClient>();
+
+        // Assert
+        settings.Should().NotBeNull();
+        
+        settings!.DnsServerSettings.Should().NotBeNull();
+        settings.DnsServerSettings!.Count.Should().Be(1);
+        settings.DnsServerSettings![0].Name.Should().Be("Local DNS Server");
+        client.Should().NotBeNull();
+        client!.NameServers.Should().NotBeNull();
+        client.NameServers!.Count.Should().Be(1);
+            
+        settings.TxtRecordSettings.Should().NotBeNull();
+        settings.TxtRecordSettings!.Hostname.Should().Be("@");
+        
+        settings.CnameRecordSettings.Should().NotBeNull();
+        settings.CnameRecordSettings!.RecordTarget.Should().Be("ownership-demo-app.com");
+    }
+}
